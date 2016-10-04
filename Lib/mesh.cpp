5e63f222d6646b805fc9c1ba164014fc1db133f9
Iverian <41ways1ucky@gmail.com>
2016-10-04 23:02:08 +0300

    добавил mesh::get_fe_node_by_three_nodes

--- Lib/mesh.cpp
+++ Lib/mesh.cpp
@@ -1,5 +1,10 @@
 #include "mesh.h"
 
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
 mesh::mesh()
     : m_node_container()
     , m_fe_container()
@@ -24,15 +29,38 @@ default_container<surface_finite_elem>& mesh::get_sfe_container()
 
 node& mesh::get_node(const id_type& p_index)
 {
-	return m_node_container.at(p_index - 1);
+    return m_node_container.at(p_index - 1);
 }
 
 finite_elem& mesh::get_fe(const id_type& p_index)
 {
-	return m_fe_container.at(p_index - 1);
+    return m_fe_container.at(p_index - 1);
 }
 
 surface_finite_elem& mesh::get_sfe(const id_type& p_index)
 {
-	return m_sfe_container.at(p_index - 1);
+    return m_sfe_container.at(p_index - 1);
+}
+
+default_container<finite_elem&> mesh::get_fe_by_three_nodes(const std::array<const node&, 3>& p_nodes)
+{
+    default_container<finite_elem&> retval;
+    for (auto i = m_fe_container.begin(); i != m_fe_container.end();) {
+        auto j = find_if(i, m_fe_container.end(),
+            [&p_nodes](const finite_elem& p_elem) -> auto {
+                auto res = true;
+                for (auto k = p_nodes.begin(); res && k != p_nodes.end(); ++k) {
+                    auto l = find(p_elem.nodes.begin(), p_elem.nodes.end(), *k);
+                    if (l == p_elem.nodes.end())
+                        res = false;
+                }
+                return res;
+            });
+        if (j != m_fe_container.end()) {
+            retval.push_back(*j);
+            advance(j, 1);
+        }
+        i = j;
+    }
+    return retval;
 }
--- Lib/mesh.h
+++ Lib/mesh.h
@@ -2,24 +2,27 @@
 
 #include "helper_class.h"
 
+#include <list>
 #include <vector>
 
 template <class T>
 using default_container = std::vector<T>;
 
-class mesh
-{
+class mesh {
 public:
-	mesh();
-	default_container<node>& get_node_container();
-	default_container<finite_elem>& get_fe_container();
-	default_container<surface_finite_elem>& get_sfe_container();
-	node& get_node(const id_type& p_index);
-	finite_elem& get_fe(const id_type& p_index);
-	surface_finite_elem& get_sfe(const id_type& p_index);
-private:
-	default_container<node> m_node_container;
-	default_container<finite_elem> m_fe_container;
-	default_container<surface_finite_elem> m_sfe_container;
-};
+    mesh();
+    default_container<node>& get_node_container();
+    default_container<finite_elem>& get_fe_container();
+    default_container<surface_finite_elem>& get_sfe_container();
 
+    node& get_node(const id_type& p_index);
+    finite_elem& get_fe(const id_type& p_index);
+    surface_finite_elem& get_sfe(const id_type& p_index);
+
+    default_container<finite_elem&> get_fe_by_three_nodes(const std::array<const node&, 3>& p_nodes);
+
+private:
+    default_container<node> m_node_container;
+    default_container<finite_elem> m_fe_container;
+    default_container<surface_finite_elem> m_sfe_container;
+};
