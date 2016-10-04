1f18f3b76a3dfe4ecedfd92981852f3cdb6aa4b2
Iverian <41ways1ucky@gmail.com>
2016-10-04 16:03:22 +0300

    настройка проекта

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,5 +1,19 @@
 #include <gtest/gtest.h>
 
+#include <ANEU_mesh_loader.h>
+
+class ANEU_mesh_loader_test : public testing::Test
+{
+public:
+	static constexpr auto aneu_test_file = "cone.aneu";
+};
+
+TEST_F(ANEU_mesh_loader_test, load_mesh_test_1)
+{
+	ANEU_mesh_loader loader;
+	loader.load_mesh(aneu_test_file);
+}
+
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
