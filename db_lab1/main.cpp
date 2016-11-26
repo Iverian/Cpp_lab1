573af60860973b849e3c2207d37f7b308cd3ae11
Iverian <41ways1ucky@gmail.com>
2016-11-26 23:22:13 +0300

    u

--- db_lab1/main.cpp
+++ db_lab1/main.cpp
@@ -1,36 +1,14 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#include "MainWindow.h"
-
-#include <QtWidgets/QApplication>
-#include <QtSql/QSqlDatabase>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-
-	auto myDb = QSqlDatabase::addDatabase("QSQLITE");
-	myDb.setDatabaseName("GHOSTBUSTERS.db");
-	myDb.open();
-
-    w.show();
-
-=======
->>>>>>> ANEU_loader
 #include "MyMainWindow.h"
+
 #include <QApplication>
 #include <QErrorMessage>
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv)
+{
     QApplication a(argc, argv);
     MyMainWindow w;
     QErrorMessage e;
     QObject::connect(&w, SIGNAL(emitError(QString)), &e, SLOT(showMessage(QString)));
     w.show();
-<<<<<<< HEAD
-=======
->>>>>>> ANEU_loader
->>>>>>> ANEU_loader
     return a.exec();
 }
